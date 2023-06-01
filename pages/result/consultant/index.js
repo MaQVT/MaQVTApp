d7f1e875038311dbd4b7273ff5df7d573dcca600
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2023-06-01 19:09:37 +0200

    Consultant Direct User

--- pages/result/consultant/index.js
+++ pages/result/consultant/index.js
@@ -85,9 +85,10 @@ export async function getServerSideProps(context) {
     const allUsersResponse = await getAllUsers()
     const clientsResponse = await getUsersByParents(userResponseJson.data._id)
     const clientsId = clientsResponse.map((value, index) => value._id.toString())
+    const consultantDirectUser = clientsResponse.filter((value, index) => value.role == "User")
     const managersResponse = allUsersResponse.filter((value, index) => clientsId.includes(value.parentId)).map((value, index) => value._id.toString())
     const usersResponse = allUsersResponse.filter((value, index) => managersResponse.includes(value.parentId)).map((value, index) => value._id.toString())
-    const mesId = [...usersResponse, ...managersResponse]
+    const mesId = [...usersResponse, ...managersResponse, ...consultantDirectUser]
     const allUsers = allUsersResponse.filter((value, index) => mesId.includes(value._id.toString()))
 
     return {
